4e373446ddb8d046843e5534277d933e5a0fddbb
kimjonghyeon6084 <ssoert123@naver.com>
2023-07-18 20:35:03 +0900

    feat:dh_japanesefood

---
+++ dh_japanesefood.js
@@ -0,0 +1,69 @@
+let jf1 = {
+  img: (src = ".dh_japanesefood_img/1스시복.jpeg"),
+  name: 스시복,
+  grade: 4.53,
+  zzim: 가게찜여부,
+};
+
+let jf2 = {
+  img: (src = ".dh_japanesefood_img/2정든그릇.jpeg"),
+  name: 정든그릇본점,
+  grade: 4.62,
+  zzim: 가게찜여부,
+};
+
+let jf3 = {
+  img: (src = ".dh_japanesefood_img/3오타코.jpeg"),
+  name: 오타코,
+  grade: "-",
+  zzim: 가게찜여부,
+};
+
+let jf4 = {
+  img: (src = ".dh_japanesefood_img/4육연타마포.jpeg"),
+  name: 육연타마포,
+  grade: "-",
+  zzim: 가게찜여부,
+};
+
+let jf5 = {
+  img: (src = ".dh_japanesefood_img/5타코방타코야끼.jpeg"),
+  name: 타코방타코야끼,
+  grade: "-",
+  zzim: 가게찜여부,
+};
+
+let jf6 = {
+  img: (src = ".dh_japanesefood_img/6멘카야.jpeg"),
+  name: 멘카야,
+  grade: 4.45,
+  zzim: 가게찜여부,
+};
+
+let jf7 = {
+  img: (src = ".dh_japanesefood_img/7톨.jpeg"),
+  name: 톨,
+  grade: 4.6,
+  zzim: 가게찜여부,
+};
+
+let jf8 = {
+  img: (src = ".dh_japanesefood_img/8경수사.jpeg"),
+  name: 경수사,
+  grade: 4.23,
+  zzim: 가게찜여부,
+};
+
+let jf9 = {
+  img: (src = ".dh_japanesefood_img/9소돌초밥.jpeg"),
+  name: 소돌초밥,
+  grade: 4.22,
+  zzim: 가게찜여부,
+};
+
+let jf10 = {
+  img: (src = ".dh_japanesefood_img/10후쿠스시.jpeg"),
+  name: 후쿠스시,
+  grade: 4.65,
+  zzim: 가게찜여부,
+};
