f7147da19d19633c41caf9a393f2b6423c770499
best0611 <best970611@naver.com>
2023-07-18 16:55:55 +0900

    feat: cafe.js added

---
+++ cafe.js
@@ -0,0 +1,70 @@
+let ca1 = {
+  img: (src = "./Cafe_img/퍼머넌트.jpeg"),
+  name: "퍼머넌트해비텟",
+  grade: "4.67",
+  tag: ["대화하기 좋은", "디저트가 맛있는"],
+  // zzim: 체크박스 selected?,
+};
+
+let ca2 = {
+  img: (src = "./Cafe_img/누아네.jpeg"),
+  name: "누아네",
+  grade: "4.46",
+  tag: ["디저트가 맛있는", "아늑한"],
+};
+
+let ca3 = {
+  img: (src = "./Cafe_img/콜로라도.jpeg"),
+  name: "콜로라도 프로젝트",
+  grade: "4.89",
+  tag: ["대화하기 좋은", "이색적인"],
+};
+
+let ca4 = {
+  img: (src = "./Cafe_img/파크사이드.jpeg"),
+  name: "파크사이드",
+  grade: "",
+  tag: ["대화하기 좋은", "넓은"],
+};
+
+let ca5 = {
+  img: (src = "./Cafe_img/이트커피.jpeg"),
+  name: "이트커피",
+  grade: "",
+  tag: ["커피가 맛있는", "이색적인"],
+};
+
+let ca6 = {
+  img: (src = "./Cafe_img/이바머스.jpeg"),
+  name: "이바머스 커피",
+  grade: "4.8",
+  tag: ["커피가 맛있는", "아늑한"],
+};
+
+let ca7 = {
+  img: (src = "./Cafe_img/카페대흥.jpeg"),
+  name: "카페 대흥",
+  grade: "4.62",
+  tag: ["대화하기 좋은", "커피가 맛있는"],
+};
+
+let ca8 = {
+  img: (src = "./Cafe_img/달쏘.jpeg"),
+  name: "달소",
+  grade: "4.67",
+  tag: ["대화하기 좋은", "아늑한"],
+};
+
+let ca9 = {
+  img: (src = "./Cafe_img/클로드.jpeg"),
+  name: "클로드 커피",
+  grade: "",
+  tag: ["대화하기 좋은", "아늑한"],
+};
+
+let ca10 = {
+  img: (src = "./Cafe_img/이어커피.jpeg"),
+  name: "이어커피 로스터스",
+  grade: "",
+  tag: ["대화하기 좋은", "커피가 맛있는"],
+};
