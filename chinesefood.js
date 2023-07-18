0b4f3ac0d564388c232f633aa42b2588624c72d5
kimjonghyeon6084 <ssoert123@naver.com>
2023-07-18 17:34:29 +0900

    feat:second1734

--- chinesefood.js
+++ chinesefood.js
@@ -1,6 +1,69 @@
-let c1 = {
-  img: 가게이미지소스,
-  name: 가게이름,
-  grade: 가게평점,
+let cf1 = {
+  img: (src = "./chinesefood_img/1샨샨/.jpeg"),
+  name: 샨샨,
+  grade: 4.44,
   zzim: 가게찜여부,
-}
+};
+
+let cf2 = {
+  img: (src = "./chinesefood_img/2신성각.pnp"),
+  name: 신성각,
+  grade: 4,
+  zzim: 가게찜여부,
+};
+
+let cf3 = {
+  img: (src = "./chinesefood_img/3금정.jpeg"),
+  name: 금정,
+  grade: 4.28,
+  zzim: 가게찜여부,
+};
+
+let cf4 = {
+  img: (src = "./chinesefood_img/4용가.jpeg"),
+  name: 용가,
+  grade: "-",
+  zzim: 가게찜여부,
+};
+
+let cf5 = {
+  img: (src = "./chinesefood_img/5쌍팔반점/.jpeg"),
+  name: 쌍팔반점,
+  grade: "-",
+  zzim: 가게찜여부,
+};
+
+let cf6 = {
+  img: (src = "./chinesefood_img/6수저가.jpeg"),
+  name: 수저가,
+  grade: 4.47,
+  zzim: 가게찜여부,
+};
+
+let cf7 = {
+  img: (src = "./chinesefood_img/7공덕132.jpeg"),
+  name: 공덕132,
+  grade: "-",
+  zzim: 가게찜여부,
+};
+
+let cf8 = {
+  img: (src = "./chinesefood_img/8청명jpeg"),
+  name: 청명,
+  grade: 4.21,
+  zzim: 가게찜여부,
+};
+
+let cf9 = {
+  img: (src = "./chinesefood_img/9세아마라탕서강대점.jpeg"),
+  name: 세아마라탕서강대점,
+  grade: 3.81,
+  zzim: 가게찜여부,
+};
+
+let cf10 = {
+  img: (src = "./chinesefood_img/10옛날짜장.jpeg"),
+  name: 옛날짜장기계우동,
+  grade: 4.51,
+  zzim: 가게찜여부,
+};
