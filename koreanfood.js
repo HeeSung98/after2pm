a7ba2196a4ff424e5f05a2ff626b83a130c9d92b
kimjonghyeon6084 <ssoert123@naver.com>
2023-07-18 18:21:30 +0900

    feat:second_koreanfood.js

---
+++ koreanfood.js
@@ -0,0 +1,62 @@
+let kf1 = {
+  img: (src = "./koreanfood_img/1황금콩밭.jpeg"),
+  name: 황금콩밭,
+  grade: 4.36,
+  zzim: 가게찜여부,
+};
+
+let kf2 = {
+  img: (src = "./koreanfood_img/2롤앤롤김밥.png"),
+  name: 롤앤롤김밥,
+  grade: "-",
+  zzim: 가게찜여부,
+};
+
+let kf3 = {
+  img: (src = "./koreanfood_img/3요수정.jpeg"),
+  name: 요수정,
+  grade: 4.4,
+  zzim: 가게찜여부,
+};
+
+let kf4 = {
+  img: (src = "./koreanfood_img/4도꼭지.jpeg"),
+  name: 도꼭지,
+  grade: 4.46,
+  zzim: 가게찜여부,
+};
+
+let kf5 = {
+  img: (src = "./koreanfood_img/5포도원삼계탕.jpeg"),
+  name: 포도원삼계탕,
+  grade: "-",
+  zzim: 가게찜여부,
+};
+
+let kf6 = {
+  img: (src = "./koreanfood_img/6계모임_마포직영점.jpeg"),
+  name: 계모임마포직영점,
+  grade: "-",
+  zzim: 가게찜여부,
+};
+
+let kf7 = {
+  img: (src = "./koreanfood_img/7밀밭정원.jpeg"),
+  name: 밀밭정원,
+  grade: 4.24,
+  zzim: 가게찜여부,
+};
+
+let kf8 = {
+  img: (src = "./koreanfood_img/8마포나룻가.jpeg"),
+  name: 마포나룻가,
+  grade: 4.07,
+  zzim: 가게찜여부,
+};
+
+let kf9 = {
+  img: (src = "./koreanfood_img/9황톳길.jpeg"),
+  name: 황톳길,
+  grade: 4.24,
+  zzim: 가게찜여부,
+};
