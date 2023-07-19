c0c9276d0494367a5ebb35db16d7e6bcb26975fe
kimjonghyeon6084 <ssoert123@naver.com>
2023-07-19 10:04:08 +0900

    feat:폴더수정

--- source/daeheung/restaurant/japanese/japanese.js
+++ source/daeheung/restaurant/japanese/japanese.js
@@ -1,68 +1,68 @@
 let dhjf1 = {
-  img: (src = ".dh_japanesefood_img/1스시복.jpeg"),
+  img: (src = "./1스시복.jepg"),
   name: 스시복,
   grade: 4.53,
   tag: ["스시킬러가인정한", "다양한사케"],
 };
 
 let dhjf2 = {
-  img: (src = ".dh_japanesefood_img/2정든그릇.jpeg"),
+  img: (src = "./2정든그릇.jepg"),
   name: 정든그릇본점,
   grade: 4.62,
   tag: ["아늑한실내", "조명맛집"],
 };
 
 let dhjf3 = {
-  img: (src = ".dh_japanesefood_img/3오타코.jpeg"),
+  img: (src = "./3오타코.jpeg"),
   name: 오타코,
   grade: "-",
   tag: ["오코노미야끼맛집", "맥주와잘어울리는"],
 };
 
 let dhjf4 = {
-  img: (src = ".dh_japanesefood_img/4육연타마포.jpeg"),
+  img: (src = "./4육연타마포.jpeg"),
   name: 육연타마포,
   grade: "-",
   tag: ["20년경력", "가성비좋은"],
 };
 
 let dhjf5 = {
-  img: (src = ".dh_japanesefood_img/5타코방타코야끼.jpeg"),
+  img: (src = "./5타코방타코야끼.jpeg"),
   name: 타코방타코야끼,
   grade: "-",
   tag: ["오픈이벤트", "오사카풍"],
 };
 
 let dhjf6 = {
-  img: (src = ".dh_japanesefood_img/6멘카야.jpeg"),
+  img: (src = "./6멘카야.jpeg"),
   name: 멘카야,
   grade: 4.45,
   tag: ["돈까스맛집", "라멘맛집"],
 };
 
 let dhjf7 = {
-  img: (src = ".dh_japanesefood_img/7톨.jpeg"),
+  img: (src = "./7톨.jpeg"),
   name: 톨,
   grade: 4.6,
   tag: ["혼밥하기좋은", "현지인맛집"],
 };
 
 let dhjf8 = {
-  img: (src = ".dh_japanesefood_img/8경수사.jpeg"),
+  img: (src = "./8경수사.jpeg"),
   name: 경수사,
   grade: 4.23,
   tag: ["깔끔한", "초밥전문점"],
 };
 
 let dhjf9 = {
-  img: (src = ".dh_japanesefood_img/9소돌초밥.jpeg"),
+  img: (src = "./9소돌초밥.jpeg"),
   name: 소돌초밥,
   grade: 4.22,
   tag: ["혼밥하기좋은", "가성비좋은"],
 };
 
 let dhjf10 = {
-  img: (src = ".dh_japanesefood_img/10후쿠스시.jpeg"),
+  img: (src = "./10후쿠스시.jpeg"),
   name: 후쿠스시,
   grade: 4.65,
   tag: ["트렌디한", "스시맛집"],
--- source/daeheung/restaurant/korean/korean.js
+++ source/daeheung/restaurant/korean/korean.js
@@ -1,61 +1,61 @@
 let dhkf1 = {
-  img: (src = "./koreanfood_img/1황금콩밭.jpeg"),
+  img: (src = "./1황금콩밭.jpeg"),
   name: 황금콩밭,
   grade: 4.36,
   tag: ["미쉐린가이드서울", "직접만드는"],
 };
 
 let dhkf2 = {
-  img: (src = "./koreanfood_img/2롤앤롤김밥.png"),
+  img: (src = "./2롤앤롤김밥.png"),
   name: 롤앤롤김밥,
   grade: "-",
   tag: ["오픈주방", "트렌드에맞는"],
 };
 
 let dhkf3 = {
-  img: (src = "./koreanfood_img/3요수정.jpeg"),
+  img: (src = "./3요수정.jpeg"),
   name: 요수정,
   grade: 4.4,
   tag: ["퓨전적인", "믿고맡김"],
 };
 
 let dhkf4 = {
-  img: (src = "./koreanfood_img/4도꼭지.jpeg"),
+  img: (src = "./4도꼭지.jpeg"),
   name: 도꼭지,
   grade: 4.46,
   tag: ["솥밥맛집", "건강식"],
 };
 
 let dhkf5 = {
-  img: (src = "./koreanfood_img/5포도원삼계탕.jpeg"),
+  img: (src = "./5포도원삼계탕.jpeg"),
   name: 포도원삼계탕,
   grade: "-",
   tag: ["보양식", "들깨맛집"],
 };
 
 let dhkf6 = {
-  img: (src = "./koreanfood_img/6계모임_마포직영점.jpeg"),
+  img: (src = "./6계모임_마포직영점.jpeg"),
   name: 계모임마포직영점,
   grade: "-",
   tag: ["모든세대도_즐길_수_있는", "갓도축한"],
 };
 
 let dhkf7 = {
-  img: (src = "./koreanfood_img/7밀밭정원.jpeg"),
+  img: (src = "./7밀밭정원.jpeg"),
   name: 밀밭정원,
   grade: 4.24,
   tag: ["매일만드는", "부드러운"],
 };
 
 let dhkf8 = {
-  img: (src = "./koreanfood_img/8마포나룻가.jpeg"),
+  img: (src = "./8마포나룻가.jpeg"),
   name: 마포나룻가,
   grade: 4.07,
   tag: ["원조김치찌개", "술안주"],
 };
 
 let dhkf9 = {
-  img: (src = "./koreanfood_img/9황톳길.jpeg"),
+  img: (src = "./9황톳길.jpeg"),
   name: 황톳길,
   grade: 4.24,
   tag: ["항아리음식", "보쌈맛집"],
