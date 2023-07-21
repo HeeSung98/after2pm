819f60328f98bf17e4c79d858063ffd1b138a9f3
HeeSung98 <wlfkrdl2@gmail.com>
2023-07-21 10:43:20 +0900

    태그 수정

--- source/yeonnam/playground/playground.js
+++ source/yeonnam/playground/playground.js
@@ -1,74 +1,74 @@
 arr.push({
-  img: "./source/yeonnam/playground/1홍대향수공방 뤼미에르퍼퓸 연남.jpeg",
-  name: "홍대향수공방<br> 뤼미에르퍼퓸",
+  img: './source/yeonnam/playground/1홍대향수공방 뤼미에르퍼퓸 연남.jpeg',
+  name: '홍대향수공방 뤼미에르퍼퓸',
   grade: 4.44,
-  tag: ["#향수공방<br>", "#서율향수공방"],
+  tag: ['#향수공방', '#서율향수공방'],
   explain:
-    "안녕하세요. 소수인원으로 운영되는 보다 집중있게 , 질높은 원데이 클래스 제공. 타 향수공방과 다른 편한 분위기에 직접 조향할수 있는 강점이 있는 공방입니다.",
-});
+    '안녕하세요. 소수인원으로 운영되는 보다 집중있게 , 질높은 원데이 클래스 제공. 타 향수공방과 다른 편한 분위기에 직접 조향할수 있는 강점이 있는 공방입니다.',
+})
 
 arr.push({
-  img: "./source/yeonnam/playground/2도토리 캐리커쳐.jpeg",
-  name: "도토리 캐리커쳐",
+  img: './source/yeonnam/playground/2도토리 캐리커쳐.jpeg',
+  name: '도토리 캐리커쳐',
   grade: 4,
-  tag: ["#캐리커쳐<br>", "#일러스트"],
+  tag: ['#캐리커쳐', '#일러스트'],
   explain:
-    "1분완성, 행복의 그림가게, 캐리커쳐 전문, 홍대 연남동 스튜디오 도로리 캐리커쳐입니다.",
-});
+    '1분완성, 행복의 그림가게, 캐리커쳐 전문, 홍대 연남동 스튜디오 도로리 캐리커쳐입니다.',
+})
 
 arr.push({
-  img: "./source/yeonnam/playground/3영메이슨 도예 스튜디오.png",
-  name: "영메이슨 도예 스튜디오",
+  img: './source/yeonnam/playground/3영메이슨 도예 스튜디오.png',
+  name: '영메이슨 도예 스튜디오',
   grade: 4.28,
-  tag: ["#연남동도예공방<br>", "#연남동도예"],
+  tag: ['#연남동도예공방', '#연남동도예'],
   explain:
-    "네이버 예약시(당일 예약x) 핸드빌딩 2인 120,000 > 110,000 / 반갑습니다. 연남동 도예공방, 영메이슨 도예 스튜디오입니다.",
-});
+    '네이버 예약시(당일 예약x) 핸드빌딩 2인 120,000 > 110,000 / 반갑습니다. 연남동 도예공방, 영메이슨 도예 스튜디오입니다.',
+})
 
 arr.push({
-  img: "./source/yeonnam/playground/4셀피스트 셀프사진관 홍대.jpeg",
-  name: "셀피스트 셀프사진관 홍대",
+  img: './source/yeonnam/playground/4셀피스트 셀프사진관 홍대.jpeg',
+  name: '셀피스트 셀프사진관 홍대',
   grade: 4.5,
-  tag: ["#셀프사진관<br>", "#커플사진"],
+  tag: ['#셀프사진관', '#커플사진'],
   explain:
-    "홍대 셀프사진관 셀피스트 스튜디오입니다. 프라이빗한 공간에서 간직하고픈 지금의 나를 남겨보세요. 연남동 테라스가 있는 셀피스트에서 뵙겠습니다.",
-});
+    '홍대 셀프사진관 셀피스트 스튜디오입니다. 프라이빗한 공간에서 간직하고픈 지금의 나를 남겨보세요. 연남동 테라스가 있는 셀피스트에서 뵙겠습니다.',
+})
 
 arr.push({
-  img: "./source/yeonnam/playground/5칠앤아트 1호점.jpeg",
-  name: "칠앤아트 1호점",
+  img: './source/yeonnam/playground/5칠앤아트 1호점.jpeg',
+  name: '칠앤아트 1호점',
   grade: 4.2,
-  tag: ["#대흥역6번출구<br>", "#마포문화생활"],
-});
+  tag: ['#대흥역6번출구', '#마포문화생활'],
+})
 
 arr.push({
-  img: "./source/yeonnam/playground/6위에트 연남.jpeg",
-  name: "위에트 연남",
+  img: './source/yeonnam/playground/6위에트 연남.jpeg',
+  name: '위에트 연남',
   grade: 4.47,
-  tag: ["#원데이클라스<br>", "#커플링공방"],
-});
+  tag: ['#원데이클라스', '#커플링공방'],
+})
 
 arr.push({
-  img: "./source/yeonnam/playground/7도토리 캐리커쳐.jpeg",
-  name: "도토리 캐리커쳐",
+  img: './source/yeonnam/playground/7도토리 캐리커쳐.jpeg',
+  name: '도토리 캐리커쳐',
   grade: 4.47,
-  tag: ["#캐리커쳐<br>", "#일러스트"],
-});
+  tag: ['#캐리커쳐', '#일러스트'],
+})
 arr.push({
-  img: "./source/yeonnam/playground/8돈룩업 연남.jpeg",
-  name: "돈룩업 연남",
+  img: './source/yeonnam/playground/8돈룩업 연남.jpeg',
+  name: '돈룩업 연남',
   grade: 4.47,
-  tag: ["#항공샷<br>", "#셀프사진관"],
-});
+  tag: ['#항공샷', '#셀프사진관'],
+})
 arr.push({
-  img: "./source/yeonnam/playground/9취미미술 그링.jpeg",
-  name: "취미미술 그링",
+  img: './source/yeonnam/playground/9취미미술 그링.jpeg',
+  name: '취미미술 그링',
   grade: 4.47,
-  tag: ["#홍대원데이클래스<br>", "#홍대취미미술"],
-});
+  tag: ['#홍대원데이클래스', '#홍대취미미술'],
+})
 arr.push({
-  img: "./source/yeonnam/playground/10플라비s 연남점.jpeg",
-  name: "플라비s 연남점",
+  img: './source/yeonnam/playground/10플라비s 연남점.jpeg',
+  name: '플라비s 연남점',
   grade: 4.47,
-  tag: ["#은팔지만들기<br>", "#향수만들기"],
-});
+  tag: ['#은팔지만들기', '#향수만들기'],
+})
